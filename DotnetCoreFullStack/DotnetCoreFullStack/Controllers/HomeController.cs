29eccfdf3cf3439cecdfefde7399f99c9244a934
MR. YASH SAWANT <yashpsawant971@gmail.com>
2025-07-06 14:14:28 +0530

    .NET Core MVC  Project Creation
    .NET Core MVC Web Application

---
+++ DotnetCoreFullStack/DotnetCoreFullStack/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using DotnetCoreFullStack.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreFullStack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
