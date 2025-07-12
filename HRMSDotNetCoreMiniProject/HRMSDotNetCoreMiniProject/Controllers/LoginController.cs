72bafc7d13814b5ecfe328627c78d23fec1443a4
MR. YASH SAWANT <yashpsawant971@gmail.com>
2025-07-12 19:07:52 +0530

    HRMS Test Task

---
+++ HRMSDotNetCoreMiniProject/HRMSDotNetCoreMiniProject/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRMSDotNetCoreMiniProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LoginPage() 
+        {
+            
+            return View();
+        
+        }
+
+    }
+}
---
+++ HRMSDotNetCoreMiniProject/HRMSDotNetCoreMiniProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HRMSDotNetCoreMiniProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+    }
+}
